1150e432553167e783680f077fffff1c057a2717
Bonaventure DOSSOU <43601208+bonaventuredossou@users.noreply.github.com>
2021-07-27 19:56:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="okwugbe",
-    version="1.0.0",
+    version="0.0.1",
     author="Bonaventure F. P. Dossou - Chris C. Emezue",
     author_email="edai.official.edai@gmail.com",
     description="Minimalist Automatic Speech Recognition Library for African Languages",
