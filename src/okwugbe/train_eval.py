9b589b549e153620ec939ed92326406dc707740b
edAI <edai.official.edai@gmail.com>
2021-12-22 07:45:39 +0100

    adding capability for commonvoice and other improvement

--- src/okwugbe/train_eval.py
+++ src/okwugbe/train_eval.py
@@ -290,13 +290,18 @@ def main(model, train_path, test_path, validation_size, learning_rate, batch_siz
 
 
 class Train_Okwugbe:
-    def __init__(self, train_path, test_path,lang=None,use_common_voice=False, characters_set=None, n_cnn=5, n_rnn=3, rnn_dim=512, num_layers=1, n_feats=128,
+    def __init__(self, train_path=None, test_path=None,lang=None,use_common_voice=False, characters_set=None, n_cnn=5, n_rnn=3, rnn_dim=512, num_layers=1, n_feats=128,
                  in_channels=1, out_channels=32, kernel=3, stride=2, padding=1, dropout=0.1, with_attention=False,
                  batch_multiplier=1, grad_acc=False, model_path='okwugbe_model', learning_rate=3e-5, batch_size=20,
                  patience=20, epochs=500, optimizer='adamw', validation_size=0.2):
         if use_common_voice==True and lang==None:
             raise Exception(f'`lang` (language from Common Voice) must be specified if use_common_voice is set to True.')
         self.common_voice = {'use_common_voice':use_common_voice,'lang':lang.strip()}    
+
+        if train_path==None and use_common_voice==False:
+            raise Exception(f'`train_path` cannot be None')
+        if test_path==None and use_common_voice==False:
+            raise Exception(f'`test_path` cannot be None')
         self.train_path = train_path
         self.test_path = test_path
         self.characters_set = characters_set if characters_set is not None else generate_character_set(lang)
