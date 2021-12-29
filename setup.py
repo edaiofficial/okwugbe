10805578c9243e80e8f8fa3fd29e60bcabf157a5
Bonaventure DOSSOU <43601208+bonaventuredossou@users.noreply.github.com>
2021-12-29 13:37:06 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="okwugbe",
-    version="0.0.4",
+    version="0.1.4",
     author="Bonaventure F. P. Dossou - Chris C. Emezue",
     author_email="edai.official.edai@gmail.com",
     description="Automatic Speech Recognition Library for African Languages",
@@ -23,4 +23,6 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=["setuptools>=42", "wheel", "numpy", "torch", "torchaudio", "datasets==1.12.1", "colorama",
+                      "ipython==5.5.0", "livelossplot", "git+https://github.com/ftyers/commonvoice-utils.git"]
 )
